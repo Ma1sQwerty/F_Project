8b6e283c4fa5a2687951fbb4c3f21eac526d9fc0
ADMIN <eliasmihalas18@gmail.com>
2023-02-24 15:30:20 +0200

    Fix #2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,16 +5,17 @@ import Youtube from '../public/SocialMedia/youtube.png'
 import Tiktok from '../public/SocialMedia/tiktok.png'
 import {  } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import Image from 'next/image'
 
 function Footer() {
   return (
     <div className='flex flex-col items-center py-10 max-w-[1920px]'>
-        <img src={Logo.src} alt="" className='h-40' />
+        <Image src={Logo.src} alt="" className='h-40' />
         <h1 className='text-xl font-bold text-white mb-5'>ALSO FIND US HERE!</h1>
         <div className='flex justify-around'>
-          <Link href='https://www.youtube.com/@JumPatras'><img src={Instagram.src} alt="" className='h-16 mx-5 cursor-pointer hover:scale-110 transition-transform duration-200'/></Link>
-          <Link href='https://www.instagram.com/jump.patras/'><img src={Youtube.src} alt="" className='h-16 mx-5 cursor-pointer hover:scale-110 transition-transform duration-200'/></Link>
-          <Link href='https://www.tiktok.com/@jump.patras'><img src={Tiktok.src} alt="" className='h-16 mx-5 cursor-pointer hover:scale-110 transition-transform duration-200'/></Link>
+          <Link href='https://www.youtube.com/@JumPatras'><Image src={Instagram.src} alt="" className='h-16 mx-5 cursor-pointer hover:scale-110 transition-transform duration-200'/></Link>
+          <Link href='https://www.instagram.com/jump.patras/'><Image src={Youtube.src} alt="" className='h-16 mx-5 cursor-pointer hover:scale-110 transition-transform duration-200'/></Link>
+          <Link href='https://www.tiktok.com/@jump.patras'><Image src={Tiktok.src} alt="" className='h-16 mx-5 cursor-pointer hover:scale-110 transition-transform duration-200'/></Link>
         </div>
     </div>
   )
--- components/RecentProjects.jsx
+++ components/RecentProjects.jsx
@@ -12,7 +12,7 @@ export default function RecentProjects(props) {
           return (
             <Link href={'/blogs/' + post.slug.current} className='px-5 py-2 bg-slate-900 mx-5 rounded-xl mb-5 xl:px-6 xl:py-5 hover:scale-[101%] transition-all duration-200'>
               <h1 className='text-white text-xl font-mono mb-4 font-bold lg:text-3xl'>{post.title}</h1>
-              <img src={urlFor(post.mainImage).url()} alt="" className='mb-2 w-full object-cover h-72 md:h-52 lg:h-64 xl:h-[25rem] 2xl:h-[27rem] rounded-xl' />
+              <Image src={urlFor(post.mainImage).url()} alt="" className='mb-2 w-full object-cover h-72 md:h-52 lg:h-64 xl:h-[25rem] 2xl:h-[27rem] rounded-xl' />
               <p className='text-white text-xl font-mono lg:text-2xl lg:my-2'>{post.description}</p>
             </Link>
           )
